58aacbe7cb21d1aa790569f346e7a5fb3a29fad2
Michael Wilkinson <miwilkin@syr.edu>
2018-09-19 11:22:43 -0400

    improve moveFiles.py help string

--- moveFiles.py
+++ moveFiles.py
@@ -20,7 +20,7 @@ class AtLeastZero(argparse.Action):
 
 
 parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter, description='move completed MC jobs to final destination. Assumes CLEAN_UP. Will ignore a given output if log/ but no data/')
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter, description='move completed MC jobs to final destination. Assumes CLEANWORK was used in run_stages.py. Will ignore a given output if log/ but no data/')
 parser.add_argument('signal_name',
                     help='name used to sort output')
 parser.add_argument('--run_sys', default='/data2',
