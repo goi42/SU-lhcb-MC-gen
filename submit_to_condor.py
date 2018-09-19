33a2bcdf97ca7825cd6df3b88fd18a327d268e9f
Michael Wilkinson <miwilkin@syr.edu>
2018-09-19 12:28:53 -0400

    submit_to_condor.py now uses configfile instead of stages and extraopts

--- submit_to_condor.py
+++ submit_to_condor.py
@@ -8,7 +8,7 @@ from moveFiles import moveFiles, runMoveFilesContinuously, parser  # some args o
 
 # -- make adjustments to parser -- #
 parser.description = '''\
-Run specified stages of run_stages.py by transferring files from store_sys to run_sys, running, then moving them back.
+Run run_stages.py using the specified configfile by transferring files from store_sys to run_sys, running, then moving them back.
 Arguments specific to this script are in the 'submit_to_condor options' group.
 This script uses a number of arguments from moveFiles.py (with some changes to default values; use --help), but it overrides some of them.
 (minallowed, maxallowed, justdata, lessthan, copyfrom, waittilnotrunning are overriden.
@@ -18,8 +18,8 @@ waittilnotrunning is only overridden for the initial movement (though it doesn't
 '''
 parser.set_defaults(interval=240, maxwaittime=0, waitcheckdelay=60, lessthan=50, waittostart=True)
 submit_to_condorgroup = parser.add_argument_group('submit_to_condor options')
-submit_to_condorgroup.add_argument('stages',
-                                   help='single string, with desired stages separated by spaces')
+submit_to_condorgroup.add_argument('configfile',
+                                   help='the configfile you want run_stages to use')
 submit_to_condorgroup.add_argument('--chunks_of', type=int, default=1000,
                                    help='how many jobs to move and submit at a time')
 submit_to_condorgroup.add_argument('--donotmovefrom', dest='transfilesfrom', action='store_false',
@@ -30,12 +30,9 @@ submit_to_condorgroup.add_argument('--setlowest', type=int, default=None,
                                    help='manually set lowest (inclusive) job number instead of letting the script find it')
 submit_to_condorgroup.add_argument('--sethighest', type=int, default=None,
                                    help='manually set highest (exclusive) job number instead of letting the script find it')
-submit_to_condorgroup.add_argument('--extraopts', default=None,
-                                   help='extra parameters to pass to run_stages.py as though from commandline, e.g., "--noCOMPRESS --noREDECAY" (--PRECLEANED and --SOME_MISSING are always used)')
-args = parser.parse_args()
 
 # -- evaluate args -- #
-stages = args.stages
+args = parser.parse_args()
 startpath = '{start_sys}/mwilkins/data/{signal_name}/'.format(start_sys=args.store_sys if args.transfilesfrom else args.run_sys, signal_name=args.signal_name if args.copyfrom is None else args.copyfrom)
 if all([args.setlowest, args.sethighest]):
     intlistdirs = range(args.setlowest, args.sethighest)
@@ -54,6 +51,24 @@ else:
 number = len(intlistdirs)
 looprange = xrange(lowest, highest, args.chunks_of)
 
+
+# -- define useful functions -- #
+def incfilename(filename, i_start=0, i=None):
+    '''chooses a name for a file by appending numbers incrementally (from i_start) to filename
+    '''
+    from os.path import exists, splitext
+    if exists(filename):
+        basename = splitext(filename)[0]
+        suffname = splitext(filename)[1]
+        newname = basename + str(i_start) + suffname
+        if exists(newname):
+            return incfilename(filename, i_start + 1)
+        else:
+            return newname
+    else:
+        return filename
+
+
 # -- submit jobs in a loop -- #
 for minnum in looprange:
     maxnum = minnum + args.chunks_of
@@ -67,26 +82,22 @@ for minnum in looprange:
             raise Exception('problem with moveFiles. [{}, {})'.format(minnum, maxnum))
     
     print 'writing condor_submit file...'
-    submissionfilename = 'MCGen_{}_{}.submit'.format(stages.replace(' ', '-'), args.signal_name)
+    submissionfilename = incfilename('MCGen.submit')
     with open(submissionfilename, 'w') as f:
-        argstring = '--SIGNAL_NAME $(SignalName) --EVENT_TYPE $(EventType) --RUN_NUMBER $(RunNumber) --FIRST_EVENT $(FirstEvent) --NUM_EVENT $(NumEvent) --GEN_LEVEL {stages} --PRECLEANED --SOME_MISSING'.format(stages=stages)
-        if args.extraopts:
-            argstring += ' ' + args.extraopts
         f.write('''\
 Executable = run_stages.py
-SignalName = {signal_name}
-EventType  = 28196040
-NumEvent   = 100
-FirstEvent = 1
+ConfigFile = {configfile}
 StartRun   = {minnum}
 RunNumber  = $$([$(StartRun)+$(process)])
-Arguments  = {argstring}
+Arguments  = $(ConfigFile) --RUN_NUMBER $(RunNumber) --PRECLEANED --SOME_MISSING
 Queue {chunks_of}
-'''.format(signal_name=args.signal_name, minnum=minnum, argstring=argstring, chunks_of=args.chunks_of))
+'''.format(configfile=args.configfile, minnum=minnum, chunks_of=args.chunks_of))
     
     print 'submitting jobs...'
     succeeded = 0 == call(['condor_submit {}'.format(submissionfilename)], shell=True)  # returns 0 if successful
-    if not succeeded:
+    if succeeded:
+        os.remove(submissionfilename)
+    else:
         raise Exception('failed to submit. [{}, {})'.format(minnum, maxnum))
     
     if args.transfilesto:
