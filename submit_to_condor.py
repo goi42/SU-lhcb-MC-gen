fe582f7f5197ee14860f2ff6720654c92c75178e
Michael Wilkinson <miwilkin@syr.edu>
2018-09-20 10:16:20 -0400

    submit_to_condor: implement runfromstorage and setlohi

--- submit_to_condor.py
+++ submit_to_condor.py
@@ -5,7 +5,7 @@ from os.path import join as opj, abspath
 from time import sleep
 import progressbar
 from imp import load_source
-from utils import incfilename
+from utils import makelohilist, incfilename
 from moveFiles import moveFiles, runMoveFilesContinuously, parser  # some args overridden--see below
 
 # -- make adjustments to parser -- #
@@ -23,22 +23,25 @@ parser.set_defaults(interval=240, maxwaittime=0, waitcheckdelay=60, lessthan=50,
 submit_to_condorgroup = parser.add_argument_group('submit_to_condor options')
 submit_to_condorgroup.add_argument('configfile', type=abspath,
                                    help='the configfile you want run_stages to use')
+submit_to_condorgroup.add_argument('--setlohi', nargs=2, type=int, default=None,
+                                   help='set the lowest (inclusive) and highest (exclusive) job numbers; overrides values found in runfromstorage')
+submit_to_condorgroup.add_argument('--runfromstorage', action='store_true',
+                                   help='move data files from store_sys to run_sys, then submit the moved run numbers (in chunks). (Make sure you use appropriate options for your configfile.)')
 submit_to_condorgroup.add_argument('--chunks_of', type=int, default=1000,
                                    help='how many jobs to move and submit at a time')
-submit_to_condorgroup.add_argument('--donotmovefrom', dest='transfilesfrom', action='store_false',
-                                   help='flag to prevent files from being moved from store_sys back to run_sys (default is misleading)')
-submit_to_condorgroup.add_argument('--donotmoveto', dest='transfilesto', action='store_false',
-                                   help='flag to prevent files from being moved from run_sys to store_sys (default is misleading)')
-submit_to_condorgroup.add_argument('--setlowest', type=int, default=None,
-                                   help='manually set lowest (inclusive) job number instead of letting the script find it')
-submit_to_condorgroup.add_argument('--sethighest', type=int, default=None,
-                                   help='manually set highest (exclusive) job number instead of letting the script find it')
+submit_to_condorgroup.add_argument('--donotstore', action='store_true',
+                                   help='flag to prevent files from being moved from run_sys to store_sys')
 submit_to_condorgroup.add_argument('--test', action='store_true',
                                    help='use error script in condor submission and preserve submission script')
 
 # -- evaluate args -- #
 allargs = parser.parse_known_args()  # unknown args are assumed to be handled by the configfile
 args = allargs[0]
+if not args.runfromstorage and args.setlohi is None:
+    raise parser.error('must runfromstorage or setlohi (or both)')
+
+cmfrom = args.copyfrom if args.movefrom is None else args.movefrom
+
 conf = load_source('conf', args.configfile)
 try:
     configparser = getattr(conf, 'parser')
@@ -47,32 +50,24 @@ except AttributeError:
     configparser = None
     args_for_configfile = ''
 
-startpath = '{start_sys}/mwilkins/data/{signal_name}/'.format(start_sys=args.store_sys if args.transfilesfrom else args.run_sys, signal_name=args.signal_name if args.copyfrom is None else args.copyfrom)
-if all([args.setlowest, args.sethighest]):
-    intlistdirs = range(args.setlowest, args.sethighest)
-else:
+if args.runfromstorage:
+    startpath = '{start_sys}/{user}/data/{signal_name}/'.format(start_sys=args.store_sys, user=args.user, signal_name=args.signal_name if cmfrom is None else cmfrom)
     intlistdirs = [int(x) for x in os.listdir(startpath) if os.path.isdir(opj(startpath, x))]
-if args.setlowest:
-    lowest = args.setlowest
-    intlistdirs = [x for x in intlistdirs if x >= lowest]
+    if args.setlohi is not None:
+        intlistdirs = [x for x in intlistdirs if args.setlohi[0] <= x < args.setlohi[1]]
 else:
-    lowest = int(min(intlistdirs))
-if args.sethighest:
-    highest = args.sethighest
-    intlistdirs = [x for x in intlistdirs if x < highest]
-else:
-    highest = int(max(intlistdirs))
+    intlistdirs = range(args.setlohi[0], args.setlohi[1])
+lowest = int(min(intlistdirs))
+highest = int(max(intlistdirs))
 number = len(intlistdirs)
-looprange = xrange(lowest, highest, args.chunks_of)
-
+submissionlist = makelohilist(intlistdirs, chunks_of)
 
 # -- submit jobs in a loop -- #
-for minnum in looprange:
-    maxnum = minnum + args.chunks_of
+for minnum, maxnum in submissionlist:
     print 'lowest = {}, highest = {}, number = {}'.format(lowest, highest, number)
     print '----------------on files in range [{}, {})-----------------'.format(minnum, maxnum)
     
-    if args.transfilesfrom:
+    if args.runfromstorage:
         print 'moving files from {} to {}...'.format(args.store_sys, args.run_sys)
         succeeded = moveFiles(store_sys=args.run_sys, run_sys=args.store_sys, minallowed=minnum, maxallowed=maxnum, justdata=True, waittilnotrunning=False, signal_name=args.signal_name, user=args.user, lessthan=args.lessthan, copyfrom=args.copyfrom)  # returns True when done
         if not succeeded:
@@ -80,6 +75,7 @@ for minnum in looprange:
     
     print 'writing condor_submit file...'
     submissionfilename = incfilename('MCGen_{}.submit'.format(signal_name))
+    Nqueue = maxnum - minnum
     with open(submissionfilename, 'w') as f:
         f.write('Executable = run_stages.py\n')
         f.write('ConfigFile = {}\n'.format(args.configfile))
@@ -88,7 +84,7 @@ for minnum in looprange:
         f.write('Arguments  = $(ConfigFile) --SIGNAL_NAME {} --RUN_NUMBER $(RunNumber) --PRECLEANED --SOME_MISSING {}\n'.format(args.signal_name, args_for_configfile))
         if args.test:
             f.write('Error      = error_{}_{}_$(RunNumber)_$(cluster)_$(process).log\n'.format(submissionfilename, args.signal_name))
-        f.write('Queue {}\n'.format(args.chunks_of))
+        f.write('Queue {}\n'.format(Nqueue))
     
     print 'submitting jobs...'
     succeeded = 0 == call(['condor_submit {}'.format(submissionfilename)], shell=True)  # returns 0 if successful
@@ -98,7 +94,7 @@ for minnum in looprange:
     else:
         raise Exception('failed to submit. [{}, {})'.format(minnum, maxnum))
     
-    if args.transfilesto:
+    if not args.donotstore:
         print 'moving files back...'
         lt = 0 if minnum == looprange[-1] else args.lessthan
         succeeded = runMoveFilesContinuously(lessthan=lt, justdata=False, minallowed=None, maxallowed=None, copyfrom=None, signal_name=args.signal_name, run_sys=args.run_sys, store_sys=args.store_sys, user=args.user, interval=args.interval, maxwaittime=args.maxwaittime, waittostart=args.waittostart, waitcheckdelay=args.waitcheckdelay, waittilnotrunning=args.waittilnotrunning, )  # returns True when done
