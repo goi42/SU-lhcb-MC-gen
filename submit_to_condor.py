8397d457620bc886c4960643696a73cf817fcf0c
Michael Wilkinson <miwilkin@syr.edu>
2018-09-20 10:29:25 -0400

    submit_to_condor: in-line doc

--- submit_to_condor.py
+++ submit_to_condor.py
@@ -43,9 +43,9 @@ if not args.runfromstorage and args.setlohi is None:
 # handle unknown arguments
 args_for_configfile = ' '.join(allargs[1])  # arguments unknown to this script as a string to pass run_stages.py
 conf = load_source('conf', args.configfile)
-try:
+try:  # if the configfile has a parser, pass arguments to it
     configparser = getattr(conf, 'parser')
-except AttributeError:
+except AttributeError:  # else don't
     configparser = None
     if args_for_configfile != '':
         raise parser.error('Unknown arguments passed and configfile has no parser! Unknown arguments: {}'.format(args_for_configfile))
@@ -53,6 +53,7 @@ except AttributeError:
 # useful definitions
 cmfrom = args.copyfrom if args.movefrom is None else args.movefrom  # will be None or a string; same logic as used in moveFiles
 
+# parse run numbers
 if args.runfromstorage:
     startpath = '{start_sys}/{user}/data/{signal_name}/'.format(start_sys=args.store_sys, user=args.user, signal_name=args.signal_name if cmfrom is None else cmfrom)
     intlistdirs = [int(x) for x in os.listdir(startpath) if os.path.isdir(opj(startpath, x))]
@@ -71,11 +72,13 @@ for minnum, maxnum in submissionlist:
     print '----------------on files in range [{}, {})-----------------'.format(minnum, maxnum)
     
     if args.runfromstorage:
+        # move files to run_sys
         print 'moving files from {} to {}...'.format(args.store_sys, args.run_sys)
         succeeded = moveFiles(store_sys=args.run_sys, run_sys=args.store_sys, minallowed=minnum, maxallowed=maxnum, justdata=True, waittilnotrunning=False, signal_name=args.signal_name, user=args.user, lessthan=args.lessthan, copyfrom=args.copyfrom)  # returns True when done
         if not succeeded:
             raise Exception('problem with moveFiles. [{}, {})'.format(minnum, maxnum))
     
+    # create condor submission file
     print 'writing condor_submit file...'
     submissionfilename = incfilename('MCGen_{}.submit'.format(signal_name))
     Nqueue = maxnum - minnum
@@ -89,6 +92,7 @@ for minnum, maxnum in submissionlist:
             f.write('Error      = error_{}_{}_$(RunNumber)_$(cluster)_$(process).log\n'.format(submissionfilename, args.signal_name))
         f.write('Queue {}\n'.format(Nqueue))
     
+    # submit jobs
     print 'submitting jobs...'
     succeeded = 0 == call(['condor_submit {}'.format(submissionfilename)], shell=True)  # returns 0 if successful
     if succeeded:
@@ -98,6 +102,7 @@ for minnum, maxnum in submissionlist:
         raise Exception('failed to submit. [{}, {})'.format(minnum, maxnum))
     
     if not args.donotstore:
+        # move files to store_sys
         print 'moving files back...'
         lt = 0 if minnum == looprange[-1] else args.lessthan
         succeeded = runMoveFilesContinuously(lessthan=lt, justdata=False, minallowed=None, maxallowed=None, copyfrom=None, signal_name=args.signal_name, run_sys=args.run_sys, store_sys=args.store_sys, user=args.user, interval=args.interval, maxwaittime=args.maxwaittime, waittostart=args.waittostart, waitcheckdelay=args.waitcheckdelay, waittilnotrunning=args.waittilnotrunning, )  # returns True when done
