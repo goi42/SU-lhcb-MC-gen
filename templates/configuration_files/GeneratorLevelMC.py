d57ab45c0a28c8bb18b953456a5ee9a3524cddc0
Michael Wilkinson <miwilkin@syr.edu>
2018-09-21 12:15:01 -0400

    Make GeneratorLevelTemplate more usable

--- templates/configuration_files/GeneratorLevelMC.py
+++ templates/configuration_files/GeneratorLevelMC.py
@@ -29,6 +29,15 @@ debuggroup.add_argument('--SCRIPT_ONLY', action='store_true',
 debuggroup.add_argument('--WORK_DIR_EXISTS', action='store_true',
                         help='BE VERY CAREFUL WHEN USING THIS FLAG: gives permission to run if WORK_DIR already exists! Also allows overwrite of extant Opts directories.')
 
+# -- parameters used for make_stage_list -- #
+# Gauss
+gaussgroup = parser.add_argument_group('Gauss parameters')
+gaussgroup.add_argument('--GAUSS_VERSION', default='<<<<default version of Gauss you want to use>>>>')
+gaussgroup.add_argument('--FIRST_EVENT', type=int, default=1)
+gaussgroup.add_argument('--NUM_EVENT', type=int, default=int('<<<<default number of events you want>>>>'))
+gaussgroup.add_argument('--EVENT_TYPE', type=int, default=int('<<<<default EVENT_TYPE you want to use>>>>'))
+
+
 # -- evaluate and check arguments -- #
 # -- mandatory section -- #
 args = parser.parse_args() if basename(__main__.__file__) == 'run_stages.py' else parser.parse_known_args()[0]  # assume all arguments are for this script if 'run_stages.py' is the main file, else allow arguments to go to other scripts
@@ -40,18 +49,62 @@ for arg in vars(args):
 # -- create stage_list (mandatory function) -- #
 def make_stage_list(USER, BASE_NAME):  # DO NOT CHANGE THIS LINE
     from datetime import datetime
-    evtnum = int('<<<<EVENTTYPE from DecFile (e.g., 15264011)>>>>')
-    stage_list = [
+    stage_list = []
+    
+    # -- Gauss stage -- #
+    GAUSS_SCRIPT_NAME = 'myGaussOpts.py'
+    GAUSS_SCRIPT_CONTENT = '''\
+# -- this script's contents are based on https://gitlab.cern.ch/lhcb-datapkg/Gen/DecFiles/blob/master/CONTRIBUTING.md -- #
+
+# -- modified $GAUSSOPTS/Gauss-Job.py -- #
+
+from Gauss.Configuration import *
+
+#--Generator phase, set random numbers
+GaussGen = GenInit("GaussGen")
+GaussGen.FirstEventNumber = {FIRST_EVENT}
+GaussGen.RunNumber        = {RUN_NUMBER}
+
+#--Number of events
+nEvts = {NUM_EVENT}
+LHCbApp().EvtMax = nEvts
+
+#Gauss().OutputType = 'NONE'
+#Gauss().Histograms = 'NONE'
+#--Set name of output files for given job (uncomment the lines)
+#  Note that if you do not set it Gauss will make a name based on event type,
+#  number of events and the date
+#idFile = 'GaussTest'
+#HistogramPersistencySvc().OutputFile = idFile+'-histos.root'
+#
+#OutputStream("GaussTape").Output = "DATAFILE='PFN:%s.sim' TYP='POOL_ROOTTREE' OPT='RECREATE'"%idFile
+
+#GenMonitor = GaudiSequencer( "GenMonitor" )
+#SimMonitor = GaudiSequencer( "SimMonitor" )
+#GenMonitor.Members += [ "GaussMonitor::CheckLifeTimeHepMC/HepMCLifeTime" ]
+#SimMonitor.Members += [ "GaussMonitor::CheckLifeTimeMC/MCLifeTime" ]
+
+# -- end modified $GAUSSOPTS/Gauss-Job.py -- #
+
+# import other standard options
+importOptions("$GAUSSOPTS/Gauss-2016.py")
+importOptions("$GAUSSOPTS/GenStandAlone.py")
+importOptions("$DECFILESROOT/options/{EVENT_TYPE}.py")  # needs to be called BEFORE setting up Pythia8, else will use Pythia6 production tool
+importOptions("$LBPYTHIA8ROOT/options/Pythia8.py")
+
+
+'''.format(RUN_NUMBER=RUN_NUMBER, FIRST_EVENT=FIRST_EVENT, NUM_EVENT=NUM_EVENT, EVENT_TYPE=EVENT_TYPE)
+    stage_list.append(
         {
             'name': 'gauss',
-            'scripts': {},
-            'log': BASE_NAME + '_gauss.log',
-            'call_string': 'lb-run -c best <<<<[--user-area <<<</path/to/your/Gauss/Build>>>>][this section only necessary if you are using a DecFile not included in the official release]>>>> Gauss/<<<<Gauss Version>>>> bash --norc; gaudirun.py /cvmfs/lhcb.cern.ch/lib/lhcb/GAUSS/GAUSS_<<<<Gauss Version>>>>/Sim/Gauss/options/Gauss-Job.py /cvmfs/lhcb.cern.ch/lib/lhcb/GAUSS/GAUSS_<<<<Gauss Version>>>>/Sim/Gauss/options/Gauss-2016.py /cvmfs/lhcb.cern.ch/lib/lhcb/GAUSS/GAUSS_<<<<Gauss Version>>>>/Sim/Gauss/options/GenStandAlone.py <<<</absolute/path/to/>>>>/Gen/DecFiles/options/{0}.py /cvmfs/lhcb.cern.ch/lib/lhcb/GAUSS/GAUSS_<<<<Gauss Version>>>>/Gen/LbPythia8/options/Pythia8.py'.format(evtnum),
+            'scripts': {GAUSS_SCRIPT_NAME: GAUSS_SCRIPT_CONTENT},
+            'log': 'mylog_gauss.log',
+            'call_string': 'lb-run -c best  <<<<[--user-area <<<</path/to/your/Gauss/Build>>>>][this section only necessary if you are using a DecFile not included in the official release]>>>> Gauss/{GAUSS_VERSION} gaudirun.py {GAUSS_SCRIPT_NAME}'.format(GAUSS_VERSION=GAUSS_VERSION, GAUSS_SCRIPT_NAME=GAUSS_SCRIPT_NAME),
             'to_remove': [],
-            'dataname': 'Gauss-{0}-5ev-{1}.xgen'.format(evtnum, str(datetime.today()).split(' ')[0].replace('-', '')),  # output filename includes reference to the date
+            'dataname': 'Gauss-{ET}-{NE}ev-{DT}.xgen'.format(ET=EVENT_TYPE, NE=NUM_EVENT, DT=str(datetime.today()).split(' ')[0].replace('-', '')),  # output filename includes reference to the date
             'run': True,
             'scriptonly': False,
         }
-    ]
+    )
         
     return stage_list
